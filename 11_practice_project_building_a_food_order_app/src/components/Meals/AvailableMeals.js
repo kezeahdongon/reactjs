d2068b125ef608764eb6236d10f302cbe4b89898
xreyc-developer <segumareyco@gmail.com>
2021-11-14 23:52:40 +0800

    adding individual Meal Items and Displaying them

--- 11_practice_project_building_a_food_order_app/src/components/Meals/AvailableMeals.js
+++ 11_practice_project_building_a_food_order_app/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,7 @@
 // imports
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
 import classes from './AvailableMeals.module.css';
 
 
@@ -30,11 +33,21 @@ const DUMMY_MEALS = [
   ];
 
 const AvailableMeals = () => {
-    const mealslist = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealslist = DUMMY_MEALS.map(meal => (
+    <MealItem 
+      key={meal.id}
+      name={meal.name} 
+      description={meal.description} 
+      price={meal.price} 
+    />
+  ));
+
     return <section className={classes.meals}>
+        <Card>
         <ul>
             {mealslist}
         </ul>
+        </Card>
     </section>
 };
 
---
+++ 11_practice_project_building_a_food_order_app/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,5 @@
+// import
+
+const MealItemForm = props => {};
+
+export default MealItemForm;
