ea0ab387828fc372e46e071cab179a2b1ea59159
xreyc-developer <segumareyco@gmail.com>
2021-11-15 21:41:27 +0800

    Using the Context

--- 11_practice_project_building_a_food_order_app/src/components/Layout/HeaderCartButton.js
+++ 11_practice_project_building_a_food_order_app/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,24 @@
 // imports
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
+import { useContext } from 'react';
 
 const HeaderCartButton = props => {
+    const cartCtx = useContext(CartContext);
+
+    /* reduce((a function that you can call) => {initial value}, starting value) allows us to transform an array of data into a single value or single number*/
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0); 
+
     return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>7</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
 };
 
