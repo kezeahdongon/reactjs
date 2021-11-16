2b36cf05534dcb018c5f4136877d2f6791deafc3
xreyc-developer <segumareyco@gmail.com>
2021-11-16 16:14:42 +0800

    Adding a Cart Reducer

--- 11_practice_project_building_a_food_order_app/src/store/CartProvider.js
+++ 11_practice_project_building_a_food_order_app/src/store/CartProvider.js
@@ -1,17 +1,45 @@
 // the purpose of this component is to manage the cart-context data and provide that context to all components that want access to it.
 
-// import
+// import from react
+import { useReducer } from 'react';
+
+// import JavaScript files
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+    items: [],
+    totatlAmount: 0
+};
 
-const CartProvider = props => {
-    const addItemToCartHandler = item => {};
+// REMEMBER: in a reducer, you receive two parameters (state, action)
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD'){
+        const updatedItems = state.items.concat(action.item); 
+        // concat() adds a new item to an array but does not edit the existing array but it return a new array.
 
-    const removeItemFromCartHandler = id => {};
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+};
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD', item: item});
+    };
+
+    const removeItemFromCartHandler = id => {
+        dispatchCartAction({type: 'REMOVE', id: id})
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     };
