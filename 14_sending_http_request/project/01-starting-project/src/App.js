81b8d0fa631743aef08ad3e8a7471f5c9b428183
xreyc-developer <segumareyco@gmail.com>
2021-11-23 21:09:17 +0800

    Using useEffect() For Requests

--- 14_sending_http_request/project/01-starting-project/src/App.js
+++ 14_sending_http_request/project/01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,8 +7,8 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-
-  async function fetchMoviesHandler() {
+  
+  const fetchMoviesHandler = useCallback( async() => {
     setIsLoading(true);
     setError(null);
     try {
@@ -33,9 +33,13 @@ function App() {
         setError(error.message);
     }
     setIsLoading(false)
-  }
+  }, []); 
 
-  const content = <p>Found no movies.</p>;
+  useEffect(() => {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
